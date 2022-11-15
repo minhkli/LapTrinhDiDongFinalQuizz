6ed112fb500374569219535b5bac8a613bb17199
Hai Minh <85656991+minhkli@users.noreply.github.com>
2022-11-15 18:29:30 +0700

    Hai Minh

--- app/src/main/java/com/example/quizzfinal/HistoryFragment.java
+++ app/src/main/java/com/example/quizzfinal/HistoryFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LevelFragment#newInstance} factory method to
+ * Use the {@link HistoryFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LevelFragment extends Fragment {
+public class HistoryFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class LevelFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public LevelFragment() {
+    public HistoryFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class LevelFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LevelFragment.
+     * @return A new instance of fragment HistoryFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LevelFragment newInstance(String param1, String param2) {
-        LevelFragment fragment = new LevelFragment();
+    public static HistoryFragment newInstance(String param1, String param2) {
+        HistoryFragment fragment = new HistoryFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class LevelFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_level, container, false);
+        return inflater.inflate(R.layout.fragment_history, container, false);
     }
 }
--- app/src/main/java/com/example/quizzfinal/MainActivity.java
+++ app/src/main/java/com/example/quizzfinal/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.quizzfinal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.quizzfinal.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NonNls;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Truy cap vao Fragment Container View
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+        //Tu Fragment Container View lay ra controller cua no
+        NavController navController = navHostFragment.getNavController();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        //Them controller vao bottonNav
+        NavigationUI.setupWithNavController(bottomNav, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.taskbar, menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //Navigation Component: Navgraph, ContainerView, Controler
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        switch (item.getItemId()) {
+            case R.id.toPlayFragment:
+                navController.navigate(R.id.playFragment);
+                return true;
+            case R.id.toHistoryFragment:
+                //Mo HelpFragment
+                navController.navigate(R.id.historyFragment);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
