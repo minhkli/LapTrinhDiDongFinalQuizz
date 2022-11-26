89f8783045de4ed0bd5ecd1a12fae974a2b9b066
Hai Minh <85656991+minhkli@users.noreply.github.com>
2022-11-26 15:25:10 +0700

    Hai Minh

--- app/src/main/java/com/example/quizzfinal/MainActivity.java
+++ app/src/main/java/com/example/quizzfinal/MainActivity.java
@@ -7,9 +7,13 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.quizzfinal.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,6 +21,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import org.jetbrains.annotations.NonNls;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnFeedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +47,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
         //Navigation Component: Navgraph, ContainerView, Controler
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         switch (item.getItemId()) {
             case R.id.toFeedbackFragment:
-                navController.navigate(R.id.playFragment);
+                Intent Email = new Intent(Intent.ACTION_SEND);
+                Email.setType("text/email");
+                Email.putExtra(Intent.EXTRA_EMAIL, new String[] { "20021556@vnu.edu.vn" });
+                Email.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+                Email.putExtra(Intent.EXTRA_TEXT, "Dear Minh" + "");
+                startActivity(Intent.createChooser(Email, "Send Feedback"));
                 return true;
             case R.id.toInformationGameFragment:
                 //Mo HelpFragment
