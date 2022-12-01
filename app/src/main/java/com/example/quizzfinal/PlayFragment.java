e682690b4446335a8e636e6d67aaf0311e5ea027
Hai Minh <85656991+minhkli@users.noreply.github.com>
2022-12-01 13:06:38 +0700

    Hai Minh

--- app/src/main/java/com/example/quizzfinal/PlayFragment.java
+++ app/src/main/java/com/example/quizzfinal/PlayFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.quizzfinal.databinding.FragmentLevelBinding;
 import com.example.quizzfinal.databinding.FragmentPlayBinding;
 
 import java.util.ArrayList;
@@ -20,35 +21,24 @@ import javax.security.auth.Subject;
 
 public class PlayFragment extends Fragment {
     FragmentPlayBinding binding;
-    RecyclerView recyclerView;
-    SendDataHome sendDataHome;
-    List<Subjects> list = new ArrayList<>();
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentPlayBinding.inflate(inflater, container, false);
-        recyclerView = binding.recycler;
-
-        Subjects subject = new Subjects("ĐỊA LÝ", R.drawable.img_geography);
-        Subjects subject1 = new Subjects("TOÁN HỌC", R.drawable._378413);
-        Subjects subject2 = new Subjects("HÓA HỌC", R.drawable.img_chem);
-        Subjects subject3 = new Subjects("LỊCH SỬ", R.drawable.img_history);
-
-        list.clear();
-        list.add(subject);
-        list.add(subject1);
-        list.add(subject2);
-        list.add(subject3);
-
-        SubjectsAdapter adapterSubjects = new SubjectsAdapter(getContext(), list, sendDataHome);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(adapterSubjects);
-
-
+        binding.imgGeo.setOnClickListener(view -> {
+            Navigation.findNavController(view).navigate(R.id.action_playFragment_to_levelFragment);
+        });
+        binding.imgMath.setOnClickListener(view -> {
+            Navigation.findNavController(view).navigate(R.id.action_playFragment_to_levelFragment);
+        });
+        binding.imgChem.setOnClickListener(view -> {
+            Navigation.findNavController(view).navigate(R.id.action_playFragment_to_levelFragment);
+        });
+        binding.imgHis.setOnClickListener(view -> {
+            Navigation.findNavController(view).navigate(R.id.action_playFragment_to_levelFragment);
+        });
         return binding.getRoot();
     }
 }
