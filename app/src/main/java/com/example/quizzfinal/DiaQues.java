fb208f8adf30a8a7f90064b65dfc0c5e243d3f67
nguyenphan0101 <nguyenphan01012002@gmail.com>
2022-12-07 09:24:36 +0700

    Phan Nguyen

--- app/src/main/java/com/example/quizzfinal/DiaQues.java
+++ app/src/main/java/com/example/quizzfinal/DiaQues.java
@@ -7,7 +7,7 @@ public class DiaQues {
             "Hà Nội là thành phố có diện tích lớn nhất Việt Nam",
             "Nghệ An là tỉnh có diện tích lớn nhất Việt Nam",
             "Nước tiếp giáp phía bắc Việt Nam là Myanma",
-            "Đông Lào"
+            "Hoàng Sa Trường Sa là của Việt Nam"
     };
 
     public static String[] easy_correct = {
@@ -19,9 +19,9 @@ public class DiaQues {
     };
 
     public static String[] medium_question = {
-            "Hà Nội là thủ đô của Việt Nam ?",
-            "Hà Nội là thành phố có diện tích lớn nhất Việt Nam",
-            "Nghệ An là tỉnh có diện tích lớn nhất Việt Nam",
+            "Hà Nội được cấp biển số 37?",
+            "Hoa Kỳ cách Việt Nam 120km?",
+            "",
             "Nước tiếp giáp phía bắc Việt Nam là Myanma",
             "Đông Lào"
     };
--- app/src/main/java/com/example/quizzfinal/HoaQues.java
+++ app/src/main/java/com/example/quizzfinal/HoaQues.java
@@ -3,49 +3,49 @@ package com.example.quizzfinal;
 public class HoaQues {
 
     public static String[] easy_question = {
-            "Hà Nội là thủ đô của Việt Nam ?",
-            "Hà Nội là thành phố có diện tích lớn nhất Việt Nam",
-            "Nghệ An là tỉnh có diện tích lớn nhất Việt Nam",
-            "Nước tiếp giáp phía bắc Việt Nam là Myanma",
-            "Đông Lào"
+            "Nước là H20?",
+            "Khí Cacbonic chiếm 0.03% thể tính không khí?",
+            "Nước có công thức là HOHO?",
+            "H2 + O2 = HOHO?",
+            "Chúng ta có thể sống thiếu nước!"
     };
 
     public static String[] easy_correct = {
             "Đúng",
-            "Sai",
             "Đúng",
             "Sai",
-            "Đúng"
+            "Sai",
+            "Sai"
     };
 
     public static String[] medium_question = {
-            "Hà Nội là thủ đô của Việt Nam ?",
-            "Hà Nội là thành phố có diện tích lớn nhất Việt Nam",
-            "Nghệ An là tỉnh có diện tích lớn nhất Việt Nam",
-            "Nước tiếp giáp phía bắc Việt Nam là Myanma",
-            "Đông Lào"
+            "H2SO4 là Axit sunfuric?",
+            "Fe + H2SO4 = FE Credit",
+            "Axit là Bazơ",
+            "Nitơ hoá trị III",
+            "Nitơ có độ âm điện 3.04 "
     };
 
     public static String[] medium_correct = {
             "Đúng",
             "Sai",
-            "Đúng",
             "Sai",
+            "Đúng",
             "Đúng"
     };
     public static String[] hard_question = {
-            "Hà Nội là thủ đô của Việt Nam ?",
-            "Hà Nội là thành phố có diện tích lớn nhất Việt Nam",
-            "Nghệ An là tỉnh có diện tích lớn nhất Việt Nam",
-            "Nước tiếp giáp phía bắc Việt Nam là Myanma",
-            "Đông Lào"
+            "mol = V/22,4",
+            "Không học Hoá là chân lý!",
+            "Hoá trị cũng các chất là như nhau",
+            "Cho 22,4l nước vào bụng, có thể xảy ra phản ứng không",
+            "C2H2 là Alkyne"
     };
 
     public static String[] hard_correct = {
             "Đúng",
-            "Sai",
             "Đúng",
             "Sai",
+            "Đúng",
             "Đúng"
     };
 }
